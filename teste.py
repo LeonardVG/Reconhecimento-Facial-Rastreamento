09bb60fc0f8dba8340769df250a3374a0c15c3a5
Leonardo Gonçalves <vieiragonsalvesleonardo123@gmail.com>
2026-06-23 18:10:15 -0300

    melhorando a visualização

--- teste.py
+++ teste.py
@@ -4,6 +4,7 @@ import numpy as np
 webcam = cv2.VideoCapture(0)                                                   #captura
 detector_faces = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # detectar face
 lbph_classifier = cv2.face.LBPHFaceRecognizer_create()                         # reconhecer
+rastreador = cv2.legacy.TrackerKCF_create()                                  # rastrear
 
 # carregar o modelo treinado
 lbph_classifier.read('lbph_classifier_1classe_leonardo.yml')
@@ -21,36 +22,31 @@ while True:
 
     #detectando face
     deteccoes = detector_faces.detectMultiScale(img_gray, scaleFactor=1.09, minNeighbors=7)
-    x,y,w,h = 0,0,0,0
-    for x, y, w, h in deteccoes:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 200, 200), 1)          # desenha o retângulo
 
-    # redimensinando para o tamanho que foi treinado
-    face_resized = cv2.resize(img_gray, (LARGURA_TREINO, ALTURA_TREINO))
-            
-    # o predict retorna (label, distancia)
-    previsao, distancia = lbph_classifier.predict(face_resized)
+    if len(deteccoes):
+        x,y,w,h = 0,0,0,0
+        for x, y, w, h in deteccoes:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 200, 200), 1)          # desenha o retângulo
 
-    texto_debug = f"Dist:{round(distancia)}"  # texto para imprimir distancia na imagem
-    if len(deteccoes)>0:
+        # redimensinando para o tamanho que foi treinado
+        face_resized = cv2.resize(img_gray, (LARGURA_TREINO, ALTURA_TREINO))
+                
+        # o predict retorna (label, distancia)
+        previsao, distancia = lbph_classifier.predict(face_resized)
+
+        texto_debug = f"Dist:{round(distancia)}"  # texto para imprimir distancia na imagem
+        
         cv2.putText(frame, texto_debug, (x, y+h+15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
 
         # Decisão
-        if previsao == 0 and distancia < LIMITE_CONFIANCA:
+        if  distancia < LIMITE_CONFIANCA:
             cv2.putText(frame, 'Leonardo', (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),1)
-                
+                    
         else:
-            cv2.putText(frame, 'Desconhecido', (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),1)
+            cv2.putText(frame, 'Não Identificado', (x, y-3), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),1)
 
     else:
-        cv2.putText(frame, texto_debug, (10,20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-
-        # Decisão
-        if previsao == 0 and distancia < LIMITE_CONFIANCA:
-            cv2.putText(frame, 'Leonardo', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-                
-        else:
-            cv2.putText(frame, 'Desconhecido', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+        cv2.putText(frame, "Nenhuma Face Encontrada", (25, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255))
 
 
     cv2.imshow('Video', frame)
